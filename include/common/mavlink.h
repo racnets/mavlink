4af176d8ef144619b5f994e76784cdc9418556fc
Mathes <faron@ubuntu.(none)>
2011-07-02 09:24:57 +0200

    modified vn message def

--- include/common/mavlink.h
+++ include/common/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Friday, April 22 2011, 10:29 UTC
+ *	 Generated on Saturday, July 2 2011, 07:19 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
--- include/huch/mavlink.h
+++ include/huch/mavlink.h
@@ -1,7 +1,7 @@
 /** @file
  *	@brief MAVLink comm protocol.
  *	@see http://pixhawk.ethz.ch/software/mavlink
- *	 Generated on Tuesday, May 17 2011, 23:45 UTC
+ *	 Generated on Saturday, July 2 2011, 07:19 UTC
  */
 #ifndef MAVLINK_H
 #define MAVLINK_H
